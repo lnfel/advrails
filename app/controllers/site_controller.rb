ecb736af5fbbebe4f2201341901962f1c2ed542f
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2019-12-20 11:53:07 +0800

    Added views for Site controller plus partials

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,4 +2,20 @@ class SiteController < ApplicationController
 	def index
 		
 	end
+
+	def about
+		
+	end
+
+	def help
+		
+	end
+
+	def contact
+		
+	end
+
+	def terms
+		
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'site#index'
+  get '/about', to: 'site#about'
+  get '/help', to: 'site#help'
+  get '/contact', to: 'site#contact'
+  get '/terms', to: 'site#terms'
+  
   get '/check.txt', to: proc {[200, {}, ['it_works']]}
 end

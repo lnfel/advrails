4df4e6b171bd780140aaf4f809a3e4d4df5f83c1
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2019-12-23 12:50:10 +0800

    app/javascript/stylesheets/application.scss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 import "bootstrap";
 import "../stylesheets/application";
 import "./custom";

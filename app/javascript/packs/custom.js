57bc803b5d48bc375c1505e50309a4160497bdc4
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2019-12-23 12:37:33 +0800

    Added bootstrap 4, jquery and popper

---
+++ app/javascript/packs/custom.js
@@ -0,0 +1,9 @@
+// Initiate tooltip and popover from bootstrap 4
+// Don't forget to import this on app/javascript/packs/application.js
+$(function() {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+$(function() {
+  $('[data-toggle="popover"]').popover();
+});
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
-const { environment } = require('@rails/webpacker')
-
-module.exports = environment
+const { environment } = require('@rails/webpacker');
+// added popper and jquery
+const webpack = require("webpack");
+environment.plugins.append(
+"Provide",
+new webpack.ProvidePlugin({
+$: "jquery",
+jQuery: "jquery",
+Popper: ["popper.js", "default"]
+})
+);
+// end of update
+module.exports = environment;

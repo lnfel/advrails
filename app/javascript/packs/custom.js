c54fa59e34031ef9bd1d1f0587bbe34aa2981017
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2020-01-06 08:29:38 +0800

    Removed confirmations controller | Change alerts to toasts components

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -2,6 +2,9 @@
 // Don't forget to import this on app/javascript/packs/application.js
 $(function() {
   $('[data-toggle="tooltip"]').tooltip();
+  // Initialize toast notifications / alerts
+  $('.toast').toast({ delay: 2000 });
+  $('.toast').toast('show');
 });
 
 $(function() {

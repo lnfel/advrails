66b9411696d030c23fc22802154725748f73e47b
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2020-01-14 21:10:51 +0800

    Remove comment on models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-#	belongs_to :type
-#  has_and_belongs_to_many :category
+	belongs_to :type
+  has_and_belongs_to_many :category
 end
--- app/models/type.rb
+++ app/models/type.rb
@@ -1,4 +1,4 @@
 class Type < ApplicationRecord
-#  has_many :category
-#  has_many :post
+  has_many :category
+  has_many :post
 end

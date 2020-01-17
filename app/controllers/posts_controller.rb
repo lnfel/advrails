4b6c9ce2219cef28a94312673c67949682965719
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2020-01-17 19:48:59 +0800

    Add form validations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,6 +30,16 @@ class PostsController < ApplicationController
 
 	def create
 		@post = Post.new(post_params)
+
+		respond_to do | format |
+			if @post.save
+				format.html { redirect_to @post, notice: 'Post was successfully created.' }
+				format.json { render :show, status: :created, location: @post }
+			else
+				format.html { render :new }
+				format.json { render json: @post.errors, status: :unprocessable_entity }
+			end
+		end
 	end
 
 	private

94e7ec94dd4c6fbff1db47efb753b20b4b0c29aa
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2020-01-24 17:15:38 +0800

    Initialize redcarpet markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+	def markdown
+		markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, hard_wrap: true, filter_html: true, autolink: true, no_intra_emphasis: true, tables: true)
+	end
 end

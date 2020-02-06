4c329a3468ab99a818960e7ec3c38770dc70f107
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2020-02-06 16:43:49 +0800

    Run migration

--- db/migrate/20200206083857_add_message_reference_to_post.rb
+++ db/migrate/20200206083857_add_message_reference_to_post.rb
@@ -1,5 +1,5 @@
 class AddMessageReferenceToPost < ActiveRecord::Migration[6.0]
   def change
-    add_reference :posts, :message, null: false, foreign_key: true
+    add_reference :posts, :message, foreign_key: true
   end
 end

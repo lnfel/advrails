ec6e354ff1bd84be8f5993adb431475b72b7a7fe
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2020-02-17 13:36:22 +0800

    Done test with UserMailer on development environment

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,15 @@
 class UserMailer < ApplicationMailer
 
-	def send_email_to_author
-		author_email = params[:post][:email]
+	def send_email_to_author(message)
+		#author_email = params[:post][:email]
+		#puts author_email
 		#author_email = "somebody@somewhere.com"
-		@message = params[:message]
+		@message = message
+		puts message.post_id
+		author_email = Post.find_by(id: message.post_id).email
+		puts author_email
+		puts message.sender_email
+		#puts
 		mail(to: author_email, subject: 'Someone left a message for you in SUpost.')
 	end
 end

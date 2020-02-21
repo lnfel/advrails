9adb696bb304bc341b71239f9bd4879dd4349ab2
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2020-02-21 13:17:16 +0800

    Modify email layout | Add email banner

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,10 +6,10 @@ class UserMailer < ApplicationMailer
 		#author_email = "somebody@somewhere.com"
 		@message = message
 		puts message.post_id
-		author_email = Post.find_by(id: message.post_id).email
-		puts author_email
+		@post = Post.find_by(id: message.post_id)
+		puts @post
 		puts message.sender_email
 		#puts
-		mail(to: author_email, subject: 'Someone left a message for you in SUpost.')
+		mail(to: @post.email, subject: 'Someone left a message for you in SUpost.')
 	end
 end
